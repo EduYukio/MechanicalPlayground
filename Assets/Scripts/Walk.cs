3c44f6050b007936d0241b2132663320d59a7561
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-21 21:51:55 -0300

    Add Walk mechanic

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour
+{
+    //private Collision coll;
+    public Rigidbody2D rb;
+    public float speed = 10;
+    public float wallJumpLerp = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //coll = GetComponent<Collision>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        //float xRaw = Input.GetAxisRaw("Horizontal");
+        //float yRaw = Input.GetAxisRaw("Vertical");
+        Vector2 dir = new Vector2(x, y);
+
+        WalkAction(dir);
+    }
+
+    private void WalkAction(Vector2 dir)
+    {
+        //rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+
+        rb.velocity = Vector2.Lerp(rb.velocity, (new Vector2(dir.x * speed, rb.velocity.y)), wallJumpLerp * Time.deltaTime);
+    }
+}
