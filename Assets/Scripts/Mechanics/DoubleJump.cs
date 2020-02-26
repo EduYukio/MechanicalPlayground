66b9ecbfc0d32bcff8a2e40769c786247b7305d4
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-26 19:47:05 -0300

    Fix DoubleJump

--- Assets/Scripts/Mechanics/DoubleJump.cs
+++ Assets/Scripts/Mechanics/DoubleJump.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoubleJump : Jump
-{
+public class DoubleJump : Jump {
     private bool hasDoubleJumped = false;
 
-    void Update()
-    {
-        if (coll.onGround)
-        {
+    void Update() {
+        if (coll.onGround) {
             hasDoubleJumped = false;
         }
-        else
-        {
-            if (isJumping && Input.GetButtonDown("Jump") && !hasDoubleJumped)
-            {
+        else {
+            if (Input.GetButtonDown("Jump") && !hasDoubleJumped) {
                 JumpAction(Vector2.up, false);
                 hasDoubleJumped = true;
             }
-
         }
     }
 }
--- Assets/Scripts/Mechanics/Jump.cs
+++ Assets/Scripts/Mechanics/Jump.cs
@@ -2,55 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Jump : MonoBehaviour
-{
+public class Jump : MonoBehaviour {
     public Rigidbody2D rb;
     public CollisionChecker coll;
     public float fallMultiplier = 3f;
     public float lowJumpMultiplier = 15f;
     public float jumpForce = 15f;
 
-
-    public static bool isJumping = false;
-
-    void Start()
-    {
+    void Start() {
         coll = GetComponent<CollisionChecker>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
-    {
-        if (Input.GetButtonDown("Jump"))
-        {
-            if (coll.onGround)
-            {
+    void Update() {
+        if (Input.GetButtonDown("Jump")) {
+            if (coll.onGround) {
                 JumpAction(Vector2.up, false);
             }
         }
-        else if(coll.onGround){
-            isJumping = false;
-        }
-
         SmoothJump();
         rb.gravityScale = 3;
     }
 
-    public void JumpAction(Vector2 dir, bool wall)
-    {
+    public void JumpAction(Vector2 dir, bool wall) {
         //rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity = dir * jumpForce;
-        isJumping = true;
     }
 
-    private void SmoothJump()
-    {
-        if (rb.velocity.y < 0)
-        {
+    private void SmoothJump() {
+        if (rb.velocity.y < 0) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
         }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
             rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
