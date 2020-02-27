8003507dbac17a6dff65846bf09769d3e421a6cf
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-26 21:26:39 -0300

    Refactor the smooth jumping into another file

--- Assets/Scripts/Mechanics/Jump.cs
+++ Assets/Scripts/Mechanics/Jump.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class Jump : MonoBehaviour {
     public Rigidbody2D rb;
     public CollisionChecker coll;
-    public float fallMultiplier = 3f;
-    public float lowJumpMultiplier = 15f;
     public float jumpForce = 15f;
-
-    public int jumpQuantity;
+    public int jumpQuantity = 0;
 
     void Start() {
         coll = GetComponent<CollisionChecker>();
         rb = GetComponent<Rigidbody2D>();
-        jumpQuantity = 0;
     }
 
     void Update() {
@@ -29,22 +25,10 @@ public class Jump : MonoBehaviour {
         if (Input.GetButtonUp("Jump")) {
             jumpQuantity++;
         }
-
-        SmoothJump();
-        rb.gravityScale = 3;
     }
 
     public void JumpAction(Vector2 dir, bool wall) {
         //rb.velocity = new Vector2(rb.velocity.x, 0);
         rb.velocity = dir * jumpForce;
-    }
-
-    private void SmoothJump() {
-        if (rb.velocity.y < 0) {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
-        }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
-            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
-        }
     }
 }
---
+++ Assets/Scripts/SmoothFall.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFall : MonoBehaviour {
+    public Rigidbody2D rb;
+    public float fallMultiplier = 3f;
+    public float lowJumpMultiplier = 15f;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        rb.gravityScale = 3;
+
+        if (rb.velocity.y < 0) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+}
