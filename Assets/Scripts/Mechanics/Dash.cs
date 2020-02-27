5ff9c5b918d447d933af5ceafa1345e5c299c179
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-26 22:24:32 -0300

    Add Dash mechanic

---
+++ Assets/Scripts/Mechanics/Dash.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour {
+    public Rigidbody2D rb;
+    public CollisionChecker coll;
+
+    public float dashSpeed = 20;
+    public bool isDashing = false;
+
+    void Start() {
+        coll = GetComponent<CollisionChecker>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float xRaw = Input.GetAxisRaw("Horizontal");
+        float yRaw = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetButtonDown("Fire2") && !isDashing) {
+            if (xRaw != 0 || yRaw != 0) {
+                DashAction(xRaw, yRaw);
+            }
+        }
+    }
+
+    private void DashAction(float x, float y) {
+        rb.velocity = Vector2.zero;
+        Vector2 dir = new Vector2(x, y);
+
+        rb.velocity = dir.normalized * dashSpeed;
+        StartCoroutine(DashWait());
+    }
+
+    IEnumerator DashWait() {
+        rb.gravityScale = 0;
+        GetComponent<SmoothFall>().enabled = false;
+        GetComponent<Walk>().enabled = false;
+        isDashing = true;
+
+        yield return new WaitForSeconds(.2f);
+
+        rb.gravityScale = 3;
+        GetComponent<SmoothFall>().enabled = true;
+        GetComponent<Walk>().enabled = true;
+        isDashing = false;
+    }
+}
