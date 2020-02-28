35188004ee4061035ec6a6ef97be9f580a0d1337
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-28 04:23:54 -0300

    Add the opening and closing of the menu hitting esc

--- Assets/Scripts/OkButton.cs
+++ Assets/Scripts/OkButton.cs
@@ -24,5 +24,6 @@ public class OkButton : MonoBehaviour {
         }
 
         menu.SetActive(false);
+        Time.timeScale = 1;
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public GameObject menu;
+
+    void Start() {
+    }
+
+    void Update() {
+        if (Input.GetButtonDown("Cancel")) {
+            OpenOrCloseMenu();
+        }
+    }
+
+    public void OpenOrCloseMenu() {
+        menu.SetActive(!menu.activeSelf);
+        if (menu.activeSelf) {
+            Time.timeScale = 0;
+        }
+        else {
+            Time.timeScale = 1;
+        }
+    }
+}
