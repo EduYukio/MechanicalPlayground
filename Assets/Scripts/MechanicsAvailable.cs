8a2211e21de73b3c926ca8eb3cc14bbabad354dd
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-02-28 03:33:18 -0300

    Add mechanics selection and script attaching on player

---
+++ Assets/Scripts/MechanicsAvailable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MechanicsAvailable : MonoBehaviour {
+    public Slots slots;
+
+    public void Start() {
+        slots = FindObjectOfType<Slots>();
+    }
+
+    public void Select() {
+        string mechanicName = EventSystem.current.currentSelectedGameObject.name;
+        slots.AddMechanic(mechanicName);
+    }
+}
---
+++ Assets/Scripts/OkButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OkButton : MonoBehaviour {
+    public Slots slots;
+    public GameObject player;
+
+    void Start() {
+        slots = FindObjectOfType<Slots>();
+        player = GameObject.Find("Player");
+    }
+
+    public void OkAction() {
+        if (slots.mechanics.Length < 3) {
+            return;
+        }
+
+        foreach (string mechanic in slots.mechanics) {
+            System.Type mType = System.Type.GetType(mechanic);
+            player.AddComponent(mType);
+        }
+
+        //disable menu
+    }
+}
