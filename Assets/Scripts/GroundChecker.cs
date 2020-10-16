569d36e657c5e2bb613678a50f4586a2bbc7a446
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-16 03:44:55 -0300

    Add a Player that can walk and jump

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour {
+    private Player playerScript;
+
+    private void Start() {
+        playerScript = FindObjectOfType<Player>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        string otherTag = other.gameObject.tag;
+        if (otherTag == "Ground") {
+            playerScript.isGrounded = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other) {
+        string otherTag = other.gameObject.tag;
+        if (otherTag == "Ground") {
+            playerScript.isGrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public bool isGrounded = false;
+    public bool disableControls = false;
+
+    [HideInInspector] public Rigidbody2D rb;
+    [HideInInspector] public float xInput;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (disableControls) return;
+
+        xInput = Input.GetAxisRaw("Horizontal");
+        ProcessJumpAction();
+    }
+
+    private void FixedUpdate() {
+        if (disableControls) return;
+
+        ProcessWalkAction();
+    }
+
+    void ProcessWalkAction() {
+        // if (!isDashing) {
+        Walk(xInput);
+        // }
+    }
+
+    void Walk(float xInput) {
+        int direction = 0;
+        if (xInput > 0) {
+            direction = 1;
+        }
+        else if (xInput < 0) {
+            direction = -1;
+        }
+        rb.velocity = new Vector2(direction * moveSpeed, rb.velocity.y);
+
+        // if (xInput < 0) {
+        //     lastDirection = -1;
+        //     spriteRenderer.flipX = true;
+        // }
+        // else if (xInput > 0) {
+        //     lastDirection = 1;
+        //     spriteRenderer.flipX = false;
+        // }
+    }
+
+    void ProcessJumpAction() {
+        if (Input.GetButtonDown("Jump")) {
+            if (isGrounded) {
+                Jump();
+            }
+            // else if (gotDoubleJumpItem && canDoubleJump) {
+            //     Jump();
+            //     canDoubleJump = false;
+            // }
+        }
+    }
+
+    void Jump() {
+        rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+}
