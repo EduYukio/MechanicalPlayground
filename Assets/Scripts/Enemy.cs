cc1b13f5ae9d169dce2d0053f5c7311cbd473f8e
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-19 04:29:21 -0300

    Add Attack mechanic

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float maxHealth = 25f;
+    public Animator animator;
+    [HideInInspector] public Rigidbody2D rb;
+    float currentHealth;
+
+    void Start() {
+        currentHealth = maxHealth;
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+
+    }
+
+    public void TakeDamage(float damage) {
+        currentHealth -= damage;
+        animator.SetTrigger("BeingHit");
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+
+    }
+
+    void Die() {
+        animator.SetBool("isDead", true);
+        rb.velocity = new Vector3(2f, 4f, 0);
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.angularVelocity = -35f;
+        rb.gravityScale = 1f;
+        GetComponent<Collider2D>().enabled = false;
+        Destroy(transform.gameObject, 1f);
+    }
+}
---
+++ Assets/Scripts/Mechanics/Attack.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+    private Player player;
+    public Animator animator;
+    public Transform attackPoint;
+    public float attackRange = 0.5f;
+    public LayerMask enemyLayers;
+    public float attackDamage = 10f;
+    public float attackRate = 2f;
+    float nextAttackTime = 0f;
+    public GameObject slashEffect;
+
+    void Start() {
+        player = FindObjectOfType<Player>();
+        animator = GetComponent<Animator>();
+        attackPoint = GameObject.FindWithTag("AttackPoint").GetComponent<Transform>();
+    }
+
+    void Update() {
+        ProcessAttackRequest();
+    }
+
+    void ProcessAttackRequest() {
+        if (Input.GetButtonDown("Attack") && Time.time >= nextAttackTime) {
+            AttackAction();
+            nextAttackTime = Time.time + 1f / attackRate;
+        }
+    }
+
+    Vector3 CalculateAttackPosition() {
+        Vector3 playerPos = player.gameObject.transform.position;
+        float xOffset = player.lastDirection * attackPoint.localPosition.x;
+        Vector3 attackPosition = playerPos + new Vector3(xOffset, attackPoint.localPosition.y, 0);
+
+        slashEffect.transform.position = attackPosition;
+        slashEffect.GetComponent<SpriteRenderer>().flipX = player.spriteRenderer.flipX;
+        return attackPosition;
+    }
+
+    void AttackAction() {
+        animator.SetTrigger("Attack");
+        Vector3 attackPosition = CalculateAttackPosition();
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPosition, attackRange, enemyLayers);
+        foreach (Collider2D enemy in hitEnemies) {
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        if (attackPoint == null) return;
+        Vector3 attackPosition = transform.position + new Vector3(attackPoint.localPosition.x, attackPoint.localPosition.y, 0);
+        if (player) {
+            attackPosition = transform.position + new Vector3(player.lastDirection * attackPoint.localPosition.x, attackPoint.localPosition.y, 0);
+        }
+        Gizmos.DrawWireSphere(attackPosition, attackRange);
+    }
+}
