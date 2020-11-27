0a4ea281564375cf2e128ac96904c8944fdc8dd4
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-27 04:51:32 -0300

    Add Spike obstacle

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            //play collect animation
+            PlayerFSM player = other.gameObject.GetComponent<PlayerFSM>();
+            player.items++;
+            Destroy(this);
+        }
+    }
+}
---
+++ Assets/Scripts/Spike.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        // check if player has protection
+        if (other.gameObject.CompareTag("Player")) {
+            PlayerFSM player = other.gameObject.GetComponent<PlayerFSM>();
+            player.Die();
+        }
+    }
+}
