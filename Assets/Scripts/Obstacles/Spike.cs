e27fbaccd3942e7238e7ac9dea47743679644b7a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-27 05:18:30 -0300

    Remove unused files/code

--- Assets/Scripts/Obstacles/Spike.cs
+++ Assets/Scripts/Obstacles/Spike.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spike : MonoBehaviour {
-    void Start() {
-
-    }
-
-    void Update() {
-
-    }
-
     private void OnCollisionEnter2D(Collision2D other) {
         // check if player has protection
         if (other.gameObject.CompareTag("Player")) {
--- Assets/Scripts/PuzzleRelated/Item.cs
+++ Assets/Scripts/PuzzleRelated/Item.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Item : MonoBehaviour {
-    void Start() {
-
-    }
-
-    void Update() {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
             //play collect animation
