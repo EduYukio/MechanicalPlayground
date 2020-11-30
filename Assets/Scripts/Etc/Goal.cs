c164846128b5599860b46cd53882df8e87cdc21b
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-11-30 19:13:26 -0300

    Add Goal

---
+++ Assets/Scripts/Etc/Goal.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour {
+    private Animator animator;
+    private PlayerFSM player;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindObjectOfType<PlayerFSM>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            animator.Play("GoalReached");
+            StartCoroutine(ChangeSceneAfterAnimation());
+        }
+    }
+
+    IEnumerator ChangeSceneAfterAnimation() {
+        player.animator.Play("PlayerJump");
+        player.freezePlayerState = true;
+        player.spriteRenderer.color = Color.white;
+        yield return null;
+        Time.timeScale = 0;
+
+        float duration = Helper.GetAnimationDuration("GoalReached", animator);
+        yield return new WaitForSecondsRealtime(duration * 2.5f);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
