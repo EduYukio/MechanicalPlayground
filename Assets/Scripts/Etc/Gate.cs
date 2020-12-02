360cdbfd59c509f19381bab7da289d90984ccf9c
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-02 08:18:39 -0300

    Add Gate object and its functionality with keys

---
+++ Assets/Scripts/Etc/Gate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            PlayerFSM player = other.gameObject.GetComponent<PlayerFSM>();
+            if (player.items > 0) {
+                // animação, som do portão sumindo
+                player.items--;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
