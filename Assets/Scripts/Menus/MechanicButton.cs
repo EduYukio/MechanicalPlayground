addfeeb8b0c8a0dc842d00b808101b3cec8d7f3a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-03 11:11:45 -0300

    Now the tutorial clips and information appear when hover a mechanic

---
+++ Assets/Scripts/Menus/MechanicButton.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using TMPro;
+using UnityEngine.UI;
+
+public class MechanicButton : MonoBehaviour {
+    public VideoClip mechanicClip;
+    public string mechanicName;
+    public string description;
+    public static MechanicsMenu menu;
+
+    public bool isUnavailable = false;
+    public bool isActive = false;
+
+    void Start() {
+        if (menu == null) menu = GameObject.FindObjectOfType<MechanicsMenu>();
+
+        GetComponent<Button>().onClick.AddListener(ClickedOnMechanic);
+    }
+
+    void Update() {
+
+    }
+
+    public void ClickedOnMechanic() {
+        if (isUnavailable) return;
+
+        if (isActive) {
+            // desativa
+            menu.mechanics.Deactivate(mechanicName);
+            isActive = false;
+        }
+        else {
+            // ativa
+            menu.mechanics.Activate(mechanicName);
+            isActive = true;
+        }
+    }
+
+    public void UpdateTutorialInfo() {
+        if (mechanicClip != null) {
+            menu.videoPlayer.clip = mechanicClip;
+            menu.videoPlayer.Play();
+            menu.rawImage.enabled = true;
+        }
+        menu.title.text = mechanicName;
+        menu.description.text = description;
+    }
+
+    public void ClearTutorialInfo() {
+        menu.videoPlayer.Stop();
+        menu.rawImage.enabled = false;
+        menu.title.text = "";
+        menu.description.text = "";
+    }
+}
---
+++ Assets/Scripts/Menus/MechanicsMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MechanicsMenu : MonoBehaviour {
+    public TMP_Text title;
+    public TMP_Text description;
+    public Mechanics mechanics;
+    public VideoPlayer videoPlayer;
+    public RawImage rawImage;
+
+    void Start() {
+        CleanInfo();
+    }
+
+    void Update() {
+
+    }
+
+    void CleanInfo() {
+        rawImage.enabled = false;
+        title.text = "";
+        description.text = "";
+    }
+}
