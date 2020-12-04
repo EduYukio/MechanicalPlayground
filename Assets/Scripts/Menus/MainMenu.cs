a322a2278f87059c3431d60a5c4cde3d971aa4cc
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-04 08:13:47 -0300

    Add MainMenu and Credits scene

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void PlayButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void CreditsButton() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void QuitButton() {
+        Application.Quit();
+    }
+
+    public void CreditsBackButton() {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
