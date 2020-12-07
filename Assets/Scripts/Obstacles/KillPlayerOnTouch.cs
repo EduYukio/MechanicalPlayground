380809e4babbd50424b0ae8e7cf523ac86b08a04
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-07 01:01:20 -0300

    Put bullet code into "Kill player" script

--- Assets/Scripts/Obstacles/KillPlayerOnTouch.cs
+++ Assets/Scripts/Obstacles/KillPlayerOnTouch.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class KillPlayerOnTouch : MonoBehaviour {
     bool isSpike = false;
+    bool isBullet = false;
 
     private void Start() {
         isSpike = gameObject.name.Contains("Spike");
+        isBullet = gameObject.name.Contains("Bullet");
     }
 
+    // spikes, enemy body
     private void OnCollisionEnter2D(Collision2D other) {
         if (other.gameObject.CompareTag("Player")) {
             if (isSpike && PlayerInvulnerableToSpike(other.gameObject)) return;
@@ -16,20 +19,42 @@ public class KillPlayerOnTouch : MonoBehaviour {
         }
     }
 
+    // saw, bullets
     private void OnTriggerEnter2D(Collider2D other) {
-        KillPlayer(other.gameObject);
+        if (isBullet) {
+            if (CheckBulletHitPlayer(other.gameObject)) return;
+            if (CheckBulletHitGround(other.gameObject)) return;
+        }
+        else {
+            KillPlayer(other.gameObject);
+        }
     }
 
     void KillPlayer(GameObject otherObject) {
-        if (otherObject.CompareTag("Player")) {
-            PlayerFSM player = otherObject.GetComponent<PlayerFSM>();
-            player.TransitionToState(player.DyingState);
-        }
+        PlayerFSM player = otherObject.GetComponent<PlayerFSM>();
+        player.TransitionToState(player.DyingState);
     }
 
     bool PlayerInvulnerableToSpike(GameObject otherObject) {
         PlayerFSM player = otherObject.GetComponent<PlayerFSM>();
         if (player.mechanics.IsEnabled("Spike Invulnerability")) return true;
+
+        return false;
+    }
+
+    bool CheckBulletHitPlayer(GameObject otherObject) {
+        if (otherObject.CompareTag("Player")) {
+            KillPlayer(otherObject);
+            Destroy(gameObject);
+            return true;
+        }
+        return false;
+    }
+    bool CheckBulletHitGround(GameObject otherObject) {
+        if (otherObject.CompareTag("Ground")) {
+            Destroy(gameObject);
+            return true;
+        }
         return false;
     }
 }
