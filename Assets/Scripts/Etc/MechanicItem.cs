22d5eac5e93187c7d7de659631067dcd4c22bcdb
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-20 18:33:18 -0300

    Begin tutorial development: Add Double Jump level

--- Assets/Scripts/Etc/MechanicItem.cs
+++ Assets/Scripts/Etc/MechanicItem.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MechanicItem : MonoBehaviour {
     public string mechanicName;
+    public UnityEvent WhenPicked;
+
 
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
             PlayerFSM player = other.gameObject.GetComponent<PlayerFSM>();
             player.mechanics.Activate(mechanicName);
-            Destroy(gameObject);
+            WhenPicked.Invoke();
+            Destroy(transform.parent.gameObject);
         }
     }
 }
