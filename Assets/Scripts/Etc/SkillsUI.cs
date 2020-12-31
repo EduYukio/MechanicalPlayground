9e3fc65d0f4b2a19756605758a189df84bd01ac5
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-12-31 15:06:28 -0300

    Begin development of SkillsUI

---
+++ Assets/Scripts/Etc/SkillsUI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SkillsUI : MonoBehaviour {
+    public TMP_Text levelObj;
+    private const int finalLevel = 15;
+
+    void Start() {
+        string currentLevelText = SceneManager.GetActiveScene().buildIndex.ToString();
+        levelObj.text = "Level: " + currentLevelText + "/" + finalLevel.ToString();
+    }
+
+    void Update() {
+
+    }
+}
