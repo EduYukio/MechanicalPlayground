6f6d49c41189b5fc47208994d5c4c159805c2f1a
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-01-06 04:17:13 -0300

    Add AudioManager

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+    public AudioMixerGroup mixerGroup;
+    public Sound[] sounds;
+
+    void Awake() {
+        foreach (Sound sound in sounds) {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.loop = sound.loop;
+
+            sound.source.outputAudioMixerGroup = mixerGroup;
+        }
+    }
+
+    public void Play(string soundName) {
+        Sound sound = Array.Find(sounds, (Predicate<Sound>)(item => item.name == soundName));
+        if (sound == null) {
+            Debug.LogWarning("Sound: " + soundName + " not found!");
+            return;
+        }
+
+        sound.source.volume = sound.volume * (1f + UnityEngine.Random.Range(-sound.volumeVariance / 2f, sound.volumeVariance / 2f));
+        sound.source.pitch = sound.pitch * (1f + UnityEngine.Random.Range(-sound.pitchVariance / 2f, sound.pitchVariance / 2f));
+
+        sound.source.Play();
+    }
+}
