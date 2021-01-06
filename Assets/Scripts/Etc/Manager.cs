ebb96f41664e8a255d6ca43b4bfd70157d2355c3
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-01-06 15:57:51 -0300

    Remove excess code from global manager

--- Assets/Scripts/Etc/Manager.cs
+++ Assets/Scripts/Etc/Manager.cs
@@ -4,26 +4,8 @@ static class Manager {
     public static AudioManager audio;
 
     static Manager() {
-        GameObject preloadObj = SafeFindObject("PreloadObject");
+        GameObject preloadObj = GameObject.Find("PreloadObject");
 
-        audio = (AudioManager)SafeComponentRetrieval(preloadObj, "AudioManager");
-    }
-
-    private static GameObject SafeFindObject(string objName) {
-        GameObject preloadObj = GameObject.Find(objName);
-        if (preloadObj == null) PrintErrors("GameObject " + objName + "  not on preload scene.");
-        return preloadObj;
-    }
-
-    private static Component SafeComponentRetrieval(GameObject preloadObj, string componentName) {
-        Component component = preloadObj.GetComponent(componentName);
-        if (component == null) PrintErrors("Component " + componentName + " not on preload scene.");
-        return component;
-    }
-
-    private static void PrintErrors(string error) {
-        Debug.Log(">>> Cannot proceed... " + error);
-        Debug.Log(">>> It is very likely you just forgot to launch");
-        Debug.Log(">>> from scene zero, the preload scene.");
+        audio = preloadObj.GetComponent<AudioManager>();
     }
 }
