d7f912f1a32808e0f3c65eb0f53a2b216a532287
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-01-06 04:18:04 -0300

    Add Preload scripts and a global manager file

---
+++ Assets/Scripts/Etc/PreloadInitializer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadInitializer {
+
+    // #if UNITY_EDITOR
+    public static int selectedScene = -2;
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    static void InitLoadingScene() {
+        int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (sceneIndex == 0) return;
+
+        selectedScene = sceneIndex;
+        SceneManager.LoadScene(0);
+    }
+    // #endif
+}
---
+++ Assets/Scripts/Etc/PreloadScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreloadScript : MonoBehaviour {
+    void Awake() {
+        DontDestroyOnLoad(gameObject);
+        Manager.audio.Play("BGM1");
+
+        // #if UNITY_EDITOR
+        if (PreloadInitializer.selectedScene > 0) {
+            SceneManager.LoadScene(PreloadInitializer.selectedScene);
+        }
+        else {
+            SceneManager.LoadScene(1);
+        }
+        // #endif
+    }
+}
