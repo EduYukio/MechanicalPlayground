bc2147d08bab57e387b6050d6e7f975b92eb9a66
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-01-09 17:50:05 -0300

    Fix bug where the checkpoint reset every time the player died

--- Assets/Scripts/Progression/Checkpoint.cs
+++ Assets/Scripts/Progression/Checkpoint.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
     private Animator animator;
-    public static Checkpoint currentCheckpoint = null;
+    public static string activatedName = "";
     public GameObject checkpointParticles;
 
     private void Awake() {
         animator = GetComponent<Animator>();
 
-        if (Checkpoint.currentCheckpoint == this) {
-            Checkpoint.currentCheckpoint.GetComponent<Animator>().Play("FlagOn");
+        if (Checkpoint.activatedName == gameObject.name) {
+            animator.Play("FlagOn");
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if (Checkpoint.currentCheckpoint == this) return;
+        if (!other.gameObject.CompareTag("Player")) return;
+        if (Checkpoint.activatedName == gameObject.name) return;
 
-        if (other.gameObject.CompareTag("Player")) {
-            DeactivateCurrentCheckpoint();
-            PlayerFSM.respawnPosition = transform.position;
-            animator.Play("FlagActivating");
-            Manager.audio.Play("Checkpoint Boing");
+        DeactivateCurrentCheckpoint();
+        PlayerFSM.respawnPosition = transform.position;
+        animator.Play("FlagActivating");
+        Manager.audio.Play("Checkpoint Boing");
 
-            float soundDelay = Helper.GetAnimationDuration("FlagActivating", animator) * 0.75f;
-            Manager.audio.PlayDelayed("Checkpoint Activated", soundDelay);
-            StartCoroutine(nameof(WaitForPlayParticles), soundDelay);
-            Checkpoint.currentCheckpoint = this;
-        }
+        float soundDelay = Helper.GetAnimationDuration("FlagActivating", animator) * 0.75f;
+        Manager.audio.PlayDelayed("Checkpoint Activated", soundDelay);
+        StartCoroutine(nameof(WaitForPlayParticles), soundDelay);
+        Checkpoint.activatedName = gameObject.name;
     }
 
     IEnumerator WaitForPlayParticles(float soundDelay) {
@@ -38,14 +37,15 @@ public class Checkpoint : MonoBehaviour {
     }
 
     public static void DeactivateCurrentCheckpoint() {
-        if (Checkpoint.currentCheckpoint != null) {
-            Checkpoint.currentCheckpoint.GetComponent<Animator>().Play("FlagOff");
+        if (Checkpoint.activatedName != "") {
+            Checkpoint activatedCheckpoint = GameObject.Find(activatedName).GetComponent<Checkpoint>();
+            activatedCheckpoint?.animator.Play("FlagOff");
+            Checkpoint.activatedName = "";
         }
     }
 
     public static void ResetCheckPointState() {
         DeactivateCurrentCheckpoint();
-        Checkpoint.currentCheckpoint = null;
         PlayerFSM.respawnPosition = Vector3.zero;
     }
 }
