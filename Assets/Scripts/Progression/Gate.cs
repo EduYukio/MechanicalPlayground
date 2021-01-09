641e0f394b3f0cccfa7c4f93a0fb8ce3131294cc
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-01-08 22:02:46 -0300

    Add Gate unlock particles

--- Assets/Scripts/Progression/Gate.cs
+++ Assets/Scripts/Progression/Gate.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Gate : MonoBehaviour {
+    public ParticleSystem gateParticles;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Player")) {
             PlayerFSM player = other.gameObject.GetComponent<PlayerFSM>();
             if (player.keys.Count > 0) {
                 Manager.audio.Play("Open Gate");
+                Instantiate(gateParticles, transform.position, Quaternion.identity);
                 ConsumeKey(player);
                 Destroy(gameObject, 0.1f);
             }
