2a1af80c14151c3a067703ee6e25a3a3a0254481
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-08-18 16:54:23 -0300

    Refactor player dashing state

--- Assets/Scripts/PlayerRelated/PlayerStates/PlayerDashingState.cs
+++ Assets/Scripts/PlayerRelated/PlayerStates/PlayerDashingState.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 
 public class PlayerDashingState : PlayerBaseState {
-    public float dashTimer;
+    private float dashTimer;
     private float originalGravity;
     private bool isEthereal;
 
     public override void EnterState(PlayerFSM player) {
-        player.animator.Play("PlayerDash");
-        Manager.audio.Play("Slash2");
         Setup(player);
+        PlayAnimation(player);
+        PlayAudio();
         DashAction(player);
     }
-
 
     public override void FixedUpdate(PlayerFSM player) {
         CheckForAirJumpInputBuffer(player);
@@ -32,13 +31,22 @@ public class PlayerDashingState : PlayerBaseState {
     private void Setup(PlayerFSM player) {
         dashTimer = player.config.startDashDurationTime;
         isEthereal = player.mechanics.IsEnabled("Ethereal Dash");
+        originalGravity = player.rb.gravityScale;
+        player.rb.gravityScale = 0f;
         player.hasResetDashTrigger = false;
     }
 
+    private void PlayAnimation(PlayerFSM player) {
+        player.animator.Play("PlayerDash");
+    }
+
+    private void PlayAudio() {
+        Manager.audio.Play("Slash2");
+    }
+
     private void DashAction(PlayerFSM player) {
-        originalGravity = player.rb.gravityScale;
-        player.rb.gravityScale = 0f;
         player.rb.velocity = new Vector2(player.lastDirection * player.config.dashSpeed, 0f);
+
         if (isEthereal) {
             player.spriteRenderer.color = new Color(1, 1, 1, player.config.etherealTransparency);
             player.gameObject.layer = LayerMask.NameToLayer("PlayerEthereal");
@@ -46,10 +54,12 @@ public class PlayerDashingState : PlayerBaseState {
     }
 
     private void StopDashing(PlayerFSM player) {
-        player.rb.gravityScale = originalGravity;
         player.rb.velocity = Vector2.zero;
+
+        player.rb.gravityScale = originalGravity;
         player.canDash = false;
         player.dashCooldownTimer = player.config.startDashCooldownTime;
+
         if (isEthereal) {
             player.spriteRenderer.color = new Color(1, 1, 1, 1);
             player.gameObject.layer = LayerMask.NameToLayer("Player"); ;
