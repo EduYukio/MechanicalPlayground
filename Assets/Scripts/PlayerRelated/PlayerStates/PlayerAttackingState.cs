cfa199354fd55632e5fd1a90ed743347dbffd0f5
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2021-08-18 15:51:29 -0300

    Refactor player attacking state

--- Assets/Scripts/PlayerRelated/PlayerStates/PlayerAttackingState.cs
+++ Assets/Scripts/PlayerRelated/PlayerStates/PlayerAttackingState.cs
@@ -1,23 +1,17 @@
 using UnityEngine;
 
 public class PlayerAttackingState : PlayerBaseState {
-    public LayerMask hitLayers;
-    public float attackTimer;
+    private bool shouldPogo;
+    private bool isRangeBoosted;
     private Vector3 attackDirection;
-    private bool isBoosted;
     private GameObject slashEffect;
-    private bool shouldPogo = false;
+    private float playerDamage;
 
     public override void EnterState(PlayerFSM player) {
-        Manager.audio.Play("Slash1");
         Setup(player);
-        PlayAttackAnimation(player);
+        PlayAnimation(player);
+        PlayAudio();
         AttackAction(player);
-    }
-
-    private void PlayAttackAnimation(PlayerFSM player) {
-        if (isBoosted) player.animator.Play("PlayerAttackingBoosted", -1, 0f);
-        else player.animator.Play("PlayerAttacking", -1, 0f);
     }
 
     public override void Update(PlayerFSM player) {
@@ -27,12 +21,36 @@ public class PlayerAttackingState : PlayerBaseState {
     }
 
     private void Setup(PlayerFSM player) {
-        hitLayers = LayerMask.GetMask("Enemies", "Obstacles", "Projectiles", "BulletEthereal");
+        shouldPogo = false;
         player.attackCooldownTimer = player.config.startAttackCooldownTime;
-        isBoosted = player.mechanics.IsEnabled("Range Boost");
+        isRangeBoosted = player.mechanics.IsEnabled("Range Boost");
         attackDirection = CalculateDirection(player);
-        slashEffect = (isBoosted) ? player.boostedSlash : player.normalSlash;
-        PositionSlashEffect(player, isBoosted);
+        slashEffect = (isRangeBoosted) ? player.boostedSlash : player.normalSlash;
+        playerDamage = player.config.attackDamage;
+        PositionSlashEffect(player, isRangeBoosted);
+    }
+
+    private void PlayAnimation(PlayerFSM player) {
+        if (isRangeBoosted) player.animator.Play("PlayerAttackingBoosted", -1, 0f);
+        else player.animator.Play("PlayerAttacking", -1, 0f);
+    }
+
+    private void PlayAudio() {
+        Manager.audio.Play("Slash1");
+    }
+
+    private void AttackAction(PlayerFSM player) {
+        Vector2 hitboxSize = CalculateHitBoxSize();
+        Vector3 attackPosition = CalculateAttackPosition(player);
+        LayerMask hitLayers = LayerMask.GetMask("Enemies", "Obstacles", "Projectiles", "BulletEthereal");
+
+        float attackRotationAngle = GetSlashAngleVector().z;
+        Collider2D[] hitTargets = Physics2D.OverlapCapsuleAll(attackPosition, hitboxSize, CapsuleDirection2D.Horizontal, attackRotationAngle, hitLayers);
+        foreach (Collider2D targetHit in hitTargets) {
+            CheckDamageEnemy(player, targetHit);
+            CheckPogo(player, targetHit);
+            CheckDestroyProjectile(player, targetHit);
+        }
     }
 
     private Vector3 CalculateDirection(PlayerFSM player) {
@@ -41,57 +59,61 @@ public class PlayerAttackingState : PlayerBaseState {
         return base.GetFourDirectionalInput(player, xInput, yInput);
     }
 
-    private void AttackAction(PlayerFSM player) {
-        Vector2 hitboxSize;
-        Vector2 attackPosition;
+    private void PositionSlashEffect(PlayerFSM player, bool isBoosted) {
         if (isBoosted) {
-            hitboxSize = new Vector2(3.65f, 3.65f) * GetBoostedSlashScale();
-            attackPosition = player.transform.position + GetBoostedSlashPosition();
+            slashEffect.transform.localPosition = GetBoostedSlashPosition();
         }
         else {
-            hitboxSize = new Vector2(3.65f, 3.65f) * GetNormalSlashScale();
-            attackPosition = player.transform.position + GetNormalSlashPosition();
+            slashEffect.transform.localPosition = GetNormalSlashPosition();
         }
-
-        float attackRotationAngle = GetSlashAngleVector().z;
-        Collider2D[] hitTargets = Physics2D.OverlapCapsuleAll(attackPosition, hitboxSize, CapsuleDirection2D.Horizontal, attackRotationAngle, hitLayers);
-        foreach (Collider2D colliderHit in hitTargets) {
-            CheckDamageEnemy(player, colliderHit);
-            CheckPogo(player, colliderHit);
-            CheckDestroyProjectile(player, colliderHit);
-        }
+        slashEffect.transform.eulerAngles = GetSlashAngleVector();
     }
 
-    private void CheckDamageEnemy(PlayerFSM player, Collider2D colliderHit) {
-        bool hitEnemy = colliderHit.gameObject.CompareTag("Enemy");
+    private Vector2 CalculateHitBoxSize() {
+        Vector2 baseHitboxSize = new Vector2(3.65f, 3.65f);
+
+        if (isRangeBoosted) return baseHitboxSize * GetBoostedSlashScale();
+        return baseHitboxSize * GetNormalSlashScale();
+    }
+
+    private Vector2 CalculateAttackPosition(PlayerFSM player) {
+        Vector3 baseAttackPosition = player.transform.position;
+
+        if (isRangeBoosted) return baseAttackPosition + GetBoostedSlashPosition();
+        return baseAttackPosition + GetNormalSlashPosition();
+    }
+
+    private void CheckDamageEnemy(PlayerFSM player, Collider2D targetHit) {
+        bool hitEnemy = targetHit.gameObject.CompareTag("Enemy");
+
         if (hitEnemy) {
-            GameObject enemyObj = colliderHit.gameObject;
-            enemyObj.GetComponent<Enemy>()?.TakeDamage(player.config.attackDamage);
+            GameObject enemyObj = targetHit.gameObject;
+            enemyObj.GetComponent<Enemy>()?.TakeDamage(playerDamage);
         }
     }
 
-    private void CheckDestroyProjectile(PlayerFSM player, Collider2D colliderHit) {
+    private void CheckPogo(PlayerFSM player, Collider2D targetHit) {
+        if (!player.mechanics.IsEnabled("Pogo Jump")) return;
+
+        bool hitEnemy = targetHit.gameObject.layer == LayerMask.NameToLayer("Enemies");
+        bool hitProjectile = targetHit.gameObject.layer == LayerMask.NameToLayer("Projectiles");
+        bool hitObstacle = targetHit.gameObject.layer == LayerMask.NameToLayer("Obstacles");
+
+        bool downwardSlash = GetSlashAngleVector().z == 270f;
+        shouldPogo = !player.isGrounded && downwardSlash && (hitEnemy || hitProjectile || hitObstacle);
+    }
+
+    private void CheckDestroyProjectile(PlayerFSM player, Collider2D targetHit) {
         if (!player.mechanics.IsEnabled("Destroy Projectile")) return;
 
-        bool hitProjectile = colliderHit.gameObject.CompareTag("Projectile");
+        bool hitProjectile = targetHit.gameObject.CompareTag("Projectile");
         if (hitProjectile) {
             Manager.audio.Play("Destroy Projectile");
-            GameObject bulletObj = colliderHit.gameObject;
+            GameObject bulletObj = targetHit.gameObject;
             EnemyBullet bullet = bulletObj.GetComponent<EnemyBullet>();
             MonoBehaviour.Instantiate(bullet.destroyBulletParticles, bullet.transform.position, Quaternion.identity);
             MonoBehaviour.Destroy(bulletObj);
         }
-    }
-
-    private void CheckPogo(PlayerFSM player, Collider2D colliderHit) {
-        if (!player.mechanics.IsEnabled("Pogo Jump")) return;
-
-        bool hitEnemy = colliderHit.gameObject.layer == LayerMask.NameToLayer("Enemies");
-        bool hitProjectile = colliderHit.gameObject.layer == LayerMask.NameToLayer("Projectiles");
-        bool hitObstacle = colliderHit.gameObject.layer == LayerMask.NameToLayer("Obstacles");
-
-        bool downwardSlash = GetSlashAngleVector().z == 270f;
-        shouldPogo = downwardSlash && !player.isGrounded && (hitEnemy || hitProjectile || hitObstacle);
     }
 
     private Vector3 GetBoostedSlashPosition() {
@@ -127,16 +149,6 @@ public class PlayerAttackingState : PlayerBaseState {
 
     private Vector2 GetNormalSlashScale() {
         return new Vector2(0.32f, 0.25f);
-    }
-
-    private void PositionSlashEffect(PlayerFSM player, bool isBoosted) {
-        if (isBoosted) {
-            slashEffect.transform.localPosition = GetBoostedSlashPosition();
-        }
-        else {
-            slashEffect.transform.localPosition = GetNormalSlashPosition();
-        }
-        slashEffect.transform.eulerAngles = GetSlashAngleVector();
     }
 
     public bool CheckTransitionToPogoing(PlayerFSM player) {
